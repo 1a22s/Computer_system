4a45a346a84649f536877f5c105e5d34a3bc62c2
Amuyuuuu363 <134367211+Amuyuuuu363@users.noreply.github.com>
2025-01-03 12:42:23 +0800

    Update WB.v

--- WB.v
+++ WB.v
@@ -8,11 +8,13 @@ module WB(
     input wire [`MEM_TO_WB_WD-1:0] mem_to_wb_bus,
 
     output wire [`WB_TO_RF_WD-1:0] wb_to_rf_bus,
+    
+    output  wire [37:0] wb_to_id,
 
     output wire [31:0] debug_wb_pc,
     output wire [3:0] debug_wb_rf_wen,
     output wire [4:0] debug_wb_rf_wnum,
-    output wire [31:0] debug_wb_rf_wdata
+    output wire [31:0] debug_wb_rf_wdata 
 );
 
     reg [`MEM_TO_WB_WD-1:0] mem_to_wb_bus_r;
@@ -45,6 +47,12 @@ module WB(
     } = mem_to_wb_bus_r;
 
     assign wb_to_rf_bus = {
+        rf_we,
+        rf_waddr,
+        rf_wdata
+    };
+
+    assign wb_to_id = {
         rf_we,
         rf_waddr,
         rf_wdata
